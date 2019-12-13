a675a0251a1c9c9d898b3e2c0f0fcfaa3566226f
EC2 Default User <ec2-user@ip-172-31-35-153.us-east-2.compute.internal>
2019-12-13 18:18:41 +0000

    update data files

--- src/Data/PopulationByBorough.js
+++ src/Data/PopulationByBorough.js
@@ -1,79 +1,66 @@
-const population1950 = [
-  {borough: "Bronx", population: 1451277},
-  {borough: "Brooklyn", population: 2738175},
-  {borough: "Manhattan", population: 1960101},
-  {borough: "Queens", population: 1550849},
-  {borough: "Staten Island", population: 191555}
+const bronx = [
+  {year: 1950, population: 1451277},
+  {year: 1960, population: 1424815},
+  {year: 1970, population: 1471701},
+  {year: 1980, population: 1168972},
+  {year: 1990, population: 1203789},
+  {year: 2000, population: 1332650},
+  {year: 2010, population: 1385108},
+  {year: 2020, population: 1446788},
+  {year: 2030, population: 1518998},
+  {year: 2040, population: 1579245}
 ]
 
-const population1960 = [
-  {borough: "Bronx", population: 1424815},
-  {borough: "Brooklyn", population: 2627319},
-  {borough: "Manhattan", population: 1698281},
-  {borough: "Queens", population: 1809578},
-  {borough: "Staten Island", population: 221991}
+const brooklyn = [
+  {year: 1950, population: 2738175},
+  {year: 1960, population: 2627319},
+  {year: 1970, population: 2602012},
+  {year: 1980, population: 2230936},
+  {year: 1990, population: 2300664},
+  {year: 2000, population: 2465326},
+  {year: 2010, population: 2552911},
+  {year: 2020, population: 2648452},
+  {year: 2030, population: 2754009},
+  {year: 2040, population: 2840525}
 ]
 
-const population1970 = [
-  {borough: "Bronx", population: 1471701},
-  {borough: "Brooklyn", population: 2602012},
-  {borough: "Manhattan", population: 1539233},
-  {borough: "Queens", population: 1986473},
-  {borough: "Staten Island", population: 295443}
+const manhattan = [
+  {year: 1950, population: 1960101},
+  {year: 1960, population: 1698281},
+  {year: 1970, population: 1539233},
+  {year: 1980, population: 1428285},
+  {year: 1990, population: 1487536},
+  {year: 2000, population: 1537195},
+  {year: 2010, population: 1585873},
+  {year: 2020, population: 1638281},
+  {year: 2030, population: 1676720},
+  {year: 2040, population: 1691617}
 ]
 
-const population1980 = [
-  {borough: "Bronx", population: 1168972},
-  {borough: "Brooklyn", population: 2230936},
-  {borough: "Manhattan", population: 1428285},
-  {borough: "Queens", population: 1891325},
-  {borough: "Staten Island", population: 352121}
+const queens = [
+  {year: 1950, population: 1550849},
+  {year: 1960, population: 1809578},
+  {year: 1970, population: 1986473},
+  {year: 1980, population: 1891325},
+  {year: 1990, population: 1951598},
+  {year: 2000, population: 2229379},
+  {year: 2010, population: 2250002},
+  {year: 2020, population: 2330295},
+  {year: 2030, population: 2373551},
+  {year: 2040, population: 2412649}
 ]
 
-const population1990 = [
-  {borough: "Bronx", population: 1203789},
-  {borough: "Brooklyn", population: 2300664},
-  {borough: "Manhattan", population: 1487536},
-  {borough: "Queens", population: 1951598},
-  {borough: "Staten Island", population: 378977}
+const statenisland = [
+  {year: 1950, population: 191555},
+  {year: 1960, population: 221991},
+  {year: 1970, population: 295443},
+  {year: 1980, population: 352121},
+  {year: 1990, population: 378977},
+  {year: 2000, population: 443728},
+  {year: 2010, population: 468730},
+  {year: 2020, population: 487155},
+  {year: 2030, population: 497749},
+  {year: 2040, population: 501109}
 ]
 
-const population2000 = [
-  {borough: "Bronx", population: 1332650},
-  {borough: "Brooklyn", population: 2465326},
-  {borough: "Manhattan", population: 1537195},
-  {borough: "Queens", population: 2229379},
-  {borough: "Staten Island", population: 443728}
-]
-
-const population2010 = [
-  {borough: "Bronx", population: 1385108},
-  {borough: "Brooklyn", population: 2552911},
-  {borough: "Manhattan", population: 1585873},
-  {borough: "Queens", population: 2250002},
-  {borough: "Staten Island", population: 468730}
-]
-
-const population2020 = [
-  {borough: "Bronx", population: 1446788},
-  {borough: "Brooklyn", population: 2648452},
-  {borough: "Manhattan", population: 1638281},
-  {borough: "Queens", population: 2330295},
-  {borough: "Staten Island", population: 487155}
-]
-
-const population2030 = [
-  {borough: "Bronx", population: 1518998},
-  {borough: "Brooklyn", population: 2754009},
-  {borough: "Manhattan", population: 1676720},
-  {borough: "Queens", population: 2373551},
-  {borough: "Staten Island", population: 497749}
-]
-
-const population2040 = [
-  {borough: "Bronx", population: 1579245},
-  {borough: "Brooklyn", population: 2840525},
-  {borough: "Manhattan", population: 1691617},
-  {borough: "Queens", population: 2412649},
-  {borough: "Staten Island", population: 501109}
-]
+export { bronx, brooklyn, manhattan, queens, statenisland };
